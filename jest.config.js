9c1158ee006b17c50de4c2e8c1604cc673e809c9
cxay1 <workjacobp@gmail.com>
2026-04-15 15:13:35 +0100

    test: add Jest config, ts-jest, more test deps

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// Jest configuration
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '<rootDir>/__tests__/**/*.test.{ts,tsx}',
+  ],
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'lib/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+};
